554caa8d63861fb5961e97fc7b5529eec3df22a6
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-19 10:03:58 +0530

    library.cpp

--- library.cpp
+++ library.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
+struct Book {
+    int id;
+    string name;
+    string author;
+};
+
 int main() {
-    string title, author;
-    int quantity;
+    Book b[10];
+    int n = 0, choice;
 
-    cout << "Enter Title: ";
-    cin >> title;
+    while (true) {
+        cout << "\n1.Add Book\n2.Display\n3.Exit\nChoice: ";
+        cin >> choice;
 
-    cout << "Enter Author: ";
-    cin >> author;
-
-    cout << "Enter Quantity: ";
-    cin >> quantity;
-
-    ofstream file("books.txt", ios::app);
-    file << title << " " << author << " " << quantity << endl;
-
-    file.close();
-
-    cout << "Book Added Successfully!";
+        if (choice == 1) {
+            cout << "Enter ID Name Author: ";
+            cin >> b[n].id >> b[n].name >> b[n].author;
+            n++;
+        }
+        else if (choice == 2) {
+            for (int i = 0; i < n; i++) {
+                cout << b[i].id << " " << b[i].name << " " << b[i].author << endl;
+            }
+        }
+        else break;
+    }
 }
