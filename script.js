a33ad35b6f06e16c779bc178db1fdf90c1b0dbd1
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-19 11:23:59 +0530

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let books = JSON.parse(localStorage.getItem("books")) || [];
 let issued = JSON.parse(localStorage.getItem("issued")) || [];
 
 function login() {
-    let role = document.getElementById("role").value;
-    localStorage.setItem("role", role);
     window.location.href = "home.html";
 }
 
@@ -11,17 +9,27 @@ function addBook() {
     let id = bookId.value;
     let name = bookName.value;
     let author = author.value;
-
-    books.push({ id, name, author });
+    books.push({id,name,author});
     localStorage.setItem("books", JSON.stringify(books));
     alert("Added");
 }
 
+function deleteBook() {
+    let id = bookId.value;
+    books = books.filter(b => b.id !== id);
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+function updateBook() {
+    let id = bookId.value;
+    books = books.map(b => b.id === id ? {id, name:bookName.value, author:author.value} : b);
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
 function searchBook() {
     let q = search.value;
-    let result = books.filter(b => b.id.includes(q) || b.name.includes(q));
-    document.getElementById("result").innerHTML =
-        result.map(b => `<p>${b.id} - ${b.name}</p>`).join("");
+    let res = books.filter(b => b.name.includes(q) || b.id.includes(q));
+    result.innerHTML = res.map(b => `${b.id} - ${b.name}`).join("<br>");
 }
 
 function issueBook() {
