c879ba2200c08fbdd8582814a64fb1891aeff2f4
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-19 11:54:51 +0530

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 let books = JSON.parse(localStorage.getItem("books")) || [];
+let issued = JSON.parse(localStorage.getItem("issued")) || [];
 
 function login() {
-    window.location.href = "dashboard.html";
-}
-
-function signup() {
-    alert("Registered!");
+    window.location.href = "home.html";
 }
 
 function addBook() {
     let id = bookId.value;
     let name = bookName.value;
     let author = author.value;
-
-    books.push({ id, name, author });
+    books.push({id,name,author});
     localStorage.setItem("books", JSON.stringify(books));
+    alert("Added");
 }
 
 function deleteBook() {
@@ -25,20 +22,26 @@ function deleteBook() {
 
 function updateBook() {
     let id = bookId.value;
-    books = books.map(b => b.id === id ? { id, name: bookName.value, author: author.value } : b);
+    books = books.map(b => b.id === id ? {id, name:bookName.value, author:author.value} : b);
     localStorage.setItem("books", JSON.stringify(books));
 }
 
 function searchBook() {
     let q = search.value;
-    let res = books.filter(b => b.name.includes(q));
-    result.innerHTML = res.map(b => `<p>${b.id} ${b.name}</p>`).join("");
+    let res = books.filter(b => b.name.includes(q) || b.id.includes(q));
+    result.innerHTML = res.map(b => `${b.id} - ${b.name}`).join("<br>");
 }
 
 function issueBook() {
-    alert("Book Issued!");
+    let id = issueId.value;
+    issued.push(id);
+    localStorage.setItem("issued", JSON.stringify(issued));
+    alert("Issued");
 }
 
 function returnBook() {
-    alert("Book Returned!");
+    let id = returnId.value;
+    issued = issued.filter(b => b !== id);
+    localStorage.setItem("issued", JSON.stringify(issued));
+    alert("Returned");
 }
