f48679497d5e6122b5f49698be08239da7745baa
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2026-04-19 11:45:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let books = JSON.parse(localStorage.getItem("books")) || [];
-let issued = JSON.parse(localStorage.getItem("issued")) || [];
 
 function login() {
-    let role = document.getElementById("role").value;
     window.location.href = "dashboard.html";
 }
 
@@ -33,21 +31,14 @@ function updateBook() {
 
 function searchBook() {
     let q = search.value;
-    let result = books.filter(b => b.id.includes(q) || b.name.includes(q));
-    document.getElementById("result").innerHTML =
-        result.map(b => `<p>${b.id} - ${b.name}</p>`).join("");
+    let res = books.filter(b => b.name.includes(q));
+    result.innerHTML = res.map(b => `<p>${b.id} ${b.name}</p>`).join("");
 }
 
 function issueBook() {
-    let id = issueId.value;
-    issued.push(id);
-    localStorage.setItem("issued", JSON.stringify(issued));
-    alert("Issued");
+    alert("Book Issued!");
 }
 
 function returnBook() {
-    let id = returnId.value;
-    issued = issued.filter(b => b !== id);
-    localStorage.setItem("issued", JSON.stringify(issued));
-    alert("Returned");
+    alert("Book Returned!");
 }
